91a5dd6f14d0f6022d1ac595c1a759f8cb1c119b
brianwlego <legobw@gmail.com>
2021-01-04 19:34:50 -0500

    wakeup done

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def wakeup
-    render json: { msg: "I'm awake!" }
+    render json: { msg: "QuizCard-backend, I'm awake!" }
   end
 
   def populate
